0e2c5696cb7eacf6effb54d9ad1659a42596e744
EdemID <ed.idrisov@mail.ru>
2022-02-15 22:00:32 +0300

    Create component classes

---
+++ adapter/restapi/src/main/java/com/tochechka/kuvyrkom/restapi/ClickController.java
@@ -0,0 +1,7 @@
+package com.tochechka.kuvyrkom.restapi;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClickController {
+}
---
+++ usecase/core/src/main/java/com/tochechka/kuvyrkom/core/BusinessLogic.java
@@ -0,0 +1,7 @@
+package com.tochechka.kuvyrkom.core;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BusinessLogic {
+}
