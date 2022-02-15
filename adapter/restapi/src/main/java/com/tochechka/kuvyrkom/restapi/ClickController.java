46b12b9ee7efdfcef19dedeffce6e38cd385ce1f
EdemID <ed.idrisov@mail.ru>
2022-02-16 00:15:39 +0300

    Realize controller and business logic

--- adapter/restapi/src/main/java/com/tochechka/kuvyrkom/restapi/ClickController.java
+++ adapter/restapi/src/main/java/com/tochechka/kuvyrkom/restapi/ClickController.java
@@ -1,7 +1,28 @@
 package com.tochechka.kuvyrkom.restapi;
 
-import org.springframework.stereotype.Controller;
+import com.tochechka.kuvyrkom.core.BusinessLogic;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/counter")
+@AllArgsConstructor
 public class ClickController {
+
+    private BusinessLogic businessLogic;
+
+    @GetMapping(value = "/click", produces = APPLICATION_JSON_VALUE)
+    public Long countClick(@RequestParam(value = "id", required = false) Long id) {
+        return businessLogic.countClick(id);
+    }
+
+    @GetMapping(value = "/show", produces = APPLICATION_JSON_VALUE)
+    public Long showCount(@RequestParam(value = "id", required = false) Long id) {
+        return businessLogic.showCount(id);
+    }
 }
--- usecase/core/src/main/java/com/tochechka/kuvyrkom/core/BusinessLogic.java
+++ usecase/core/src/main/java/com/tochechka/kuvyrkom/core/BusinessLogic.java
@@ -1,7 +1,26 @@
 package com.tochechka.kuvyrkom.core;
 
+import com.tochechka.kuvyrkom.model.Click;
+import com.tochechka.kuvyrkom.persistence.service.ClickServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class BusinessLogic {
+
+    private ClickServiceImpl clickService;
+
+    public Long countClick(Long id) {
+        Click click = clickService.findById(id);
+        Long count = click.getCount();
+        Long currentCount = ++count;
+        click.setCount(currentCount);
+        clickService.save(click);
+        return currentCount;
+    }
+
+    public Long showCount(Long id) {
+        return clickService.findById(id).getCount();
+    }
 }
